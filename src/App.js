cedc542b8ef6d53685a4954f30926d9295c97bca
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-01-04 17:46:48 +0100

    сделал шаг 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import  {Component} from "react";
+// const Button = () => <button>Good</button>;
 
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  countTotalFeedback = () => { };
+  countPositiveFeedbackPercentage = () => { };
 
+  render() {
+    return (
+      <>
+        <h2>Please leave feedback</h2>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Nad</button>
+        <h2>Statistics</h2>
+        <ul>
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Nad: {this.state.bad}</li>
+        </ul>
+      </>
+    );
 
-function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  }
 }
 
-export default App;
+
